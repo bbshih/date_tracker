acf265506082586ece4a9df9ad6432f7be97111b
Billy Shih <me@billyshih.com>
2013-05-15 08:17:26 -0500

    Setup rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DateTracker::Application.config.secret_token = 'da0aca01e74835cfa24c385b20667cd8d8d9171d61943c1712f72965a620fe3b5d0e7a69a808c76b4d24a24ad3bd0007d2facee0b3d9a31fc63853e8a996727a'
